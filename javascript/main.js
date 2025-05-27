85c1d1eab8df1a698c00f854dcd8f4d2a264efae
Samhitha Singirikonda <samhithas05@gmail.com>
2025-05-27 18:38:47 +0530

    javascript commit

---
+++ javascript/main.js
@@ -0,0 +1,7 @@
+// JavaScript for Community Event Portal
+console.log("Welcome to the Community Portal");
+window.addEventListener('load', () => {
+  alert("Page is fully loaded");
+});
+
+// Additional JavaScript logic should be added here
